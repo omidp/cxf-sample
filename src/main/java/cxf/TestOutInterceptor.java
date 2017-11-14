6412c3d4a4ce3f4527ad03333e44767a6b3321af
omidp <opourhadi@infotech-co.net>
2017-11-14 09:43:31 +0330

    add out interceptor

--- src/main/java/cxf/TestOutInterceptor.java
+++ src/main/java/cxf/TestOutInterceptor.java
@@ -1,14 +1,19 @@
 package cxf;
 
-import java.io.ByteArrayInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Writer;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.cxf.interceptor.Fault;
-import org.apache.cxf.interceptor.LoggingInInterceptor;
 import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import org.apache.cxf.io.CacheAndWriteOutputStream;
 import org.apache.cxf.io.CachedOutputStream;
+import org.apache.cxf.io.CachedOutputStreamCallback;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
@@ -19,39 +24,57 @@ public class TestOutInterceptor extends AbstractPhaseInterceptor<Message>
     public TestOutInterceptor()
     {
         super(Phase.PRE_STREAM);
-        addBefore(LoggingOutInterceptor.class.getName());
+        addAfter(LoggingOutInterceptor.class.getName());
     }
 
     @Override
     public void handleMessage(Message message) throws Fault
     {
         System.out.println("MESSAGE SENT");
+        final OutputStream os = message.getContent(OutputStream.class);
+        final Writer iowriter = message.getContent(Writer.class);
+        if (os == null && iowriter == null)
+        {
+            return;
+        }
+        final CacheAndWriteOutputStream newOut = new CacheAndWriteOutputStream(os);
+        message.setContent(OutputStream.class, newOut);
+        newOut.registerCallback(new LoggerCallBack(message, os));
+        
 
-//        InputStream is = message.getContent(InputStream.class);
-//
-//        if (is != null)
-//        {
-//            CachedOutputStream bos = new CachedOutputStream();
-//            try
-//            {
-//                IOUtils.copy(is, bos);
-//                String soapMessage = new String(bos.getBytes());
-//                System.out.println("-------------------------------------------");
-//                System.out.println("incoming message is " + soapMessage);
-//                System.out.println("-------------------------------------------");
-//                bos.flush();
-//                message.setContent(InputStream.class, is);
-//
-//                is.close();
-//                InputStream inputStream = new ByteArrayInputStream(soapMessage.getBytes());
-//                message.setContent(InputStream.class, inputStream);
-//                bos.close();
-//            }
-//            catch (IOException ioe)
-//            {
-//                ioe.printStackTrace();
-//            }
-//        }
+    }
+
+    private class LoggerCallBack implements CachedOutputStreamCallback
+    {
+
+        private Message message;
+        private OutputStream origOs;
+
+        public LoggerCallBack(Message message, OutputStream os)
+        {
+            this.message = message;
+            this.origOs = os;
+        }
+
+        @Override
+        public void onClose(CachedOutputStream os)
+        {
+            try
+            {
+                InputStream is = os.getInputStream();
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+            message.setContent(OutputStream.class, this.origOs);
+        }
+
+        @Override
+        public void onFlush(CachedOutputStream os)
+        {
+
+        }
 
     }
 
